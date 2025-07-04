3f7e69e15934617422942faa8bcf5c1c76a2ce1e
Josep Audenis <pepaudenisr@gmail.com>
2025-07-04 11:01:57 +0200

    args modified

--- src/main.py
+++ src/main.py
@@ -5,17 +5,15 @@ from features.extract_features import generate_features_dataset
 
 def main():
 	parser = argparse.ArgumentParser(prog="EuroSAT classifier")
-	parser.add_argument("-t", "--train", type=str, help="", choices=["classic", "neural network"], nargs=1)
-	parser.add_argument("-e", "--extract", type=str, help="Specifiy the output filename (without extension), it will be stored in the /data/interim/ folder.", nargs=1)
+    parser.add_argument("-t", "--train", type=str, help="Trains a classical model on the specified dataset. Specify the dataset filename stored as /data/interim/(filename).npz", nargs=1)
+	parser.add_argument("-e", "--extract", type=str, help="Extracts specified featyres from the image dataset. Specifiy the output filename (without extension), it will be stored in the /data/interim/ folder.", nargs=1)
 
 	args = parser.parse_args()
 
 	if args.extract:
 		generate_features_dataset()
-	elif args.train == ['classic']:
+    elif args.train:
 		train_classic()
-	elif args.train == ["nerual network"]:
-		print("train nn")
 	else:
 		parser.print_usage()
 
